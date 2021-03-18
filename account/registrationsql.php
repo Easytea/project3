fcf46fff93dfa338df85f6915d7be7fdffbbce11
Tom Gieben <0085700@rocdeleijgraaf.nl>
2021-03-18 14:52:42 +0100

    Versie 3.0

---
+++ account/registrationsql.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+header('refresh: 2; URL=http://localhost/project3/account/login.php');//2 sec delay
+
+
+$con = mysqli_connect('localhost', 'root', '123');
+
+mysqli_select_db($con, 'userregistration');
+
+$name = $_POST['gebruikersnaam'];
+$ww = $_POST['wachtwoord'];
+$email = $_POST['email'];
+
+$s = " select * from registratie where gebruikersnaam = '$name'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    echo "Gebruikersnaam is al in gebruik";
+} else {
+    $reg= "insert into registratie(gebruikersnaam,	wachtwoord, email) values ('$name', '$ww', '$email') ";
+    mysqli_query($con, $reg);
+    echo "Registratie Succesvol";
+}
+
+?>
